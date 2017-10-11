a0dfb2e0f19de658dd84f5356d6aeedfa4f463d4
Aidan Harrington <b31277@mcast.org.mt>
2017-10-11 13:31:21 +0200

    Number Wizard

---
+++ Number_Wizard/Assets/Scripts/Increment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Increment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        num = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            num++;
+        }
+
+        //inputting down arrow
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            num--;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            num--10;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            num++10;
+        }
+    }
+}
---
+++ Number_Wizard/Assets/Scripts/Number_Wizard.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Number_Wizard : MonoBehaviour {
+
+    //Creates 2 integer values
+    int min;
+    int max;
+    int guess;
+    //type/name/value
+
+    void NextGuess()
+    {
+        print("Is number bigger or smaller than " + guess + " ?");
+        print("UP: Bigger, DOWN: Smaller, RETURN: Equal");
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;//Prints these once
+        //Debug.Log and print is the same thing
+        print("Welcome to Number Wizard");
+        Debug.Log("Please think of a number from " + min + " to " + max);
+        NextGuess();
+       
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //We use Input when we need something from the user
+        //inputting up arrow
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up Arrow pressed");
+            min = guess;
+            guess = (min + max) / 2;
+            NextGuess();
+        }
+
+        //inputting down arrow
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down Arrow pressed");
+            max = guess;
+            guess = (min + max) / 2;
+            NextGuess();
+        }
+
+        //inputting all the enters
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("YOU WIN!");
+            print("Do you want to continue");
+            print("If YES:P, No:Q");
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                StartGame();
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+        }
+    }
+}
