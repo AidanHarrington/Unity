4829a0a822d3d2eecb5599a123f52b80e19fe194
Aidan Harrington <b31277@mcast.org.mt>
2017-10-23 14:02:14 +0200

    fysdvgluei

---
+++ Number_Wizard_UI/Assets/Scripts/Level_Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Manager : MonoBehaviour {
+    
+    public  void LoadLevel(string levelName)
+    {
+        print("Loading Level " + levelName);
+        //loads the scene
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
---
+++ Number_Wizard_UI/Assets/Scripts/Number_Wizard.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Number_Wizard : MonoBehaviour {
+
+    //Creates 2 integer values
+    int min;
+    int max;
+    int guess;
+    int maxNumberOfGuesses = 10;
+
+    public Text guessText;
+
+    void NextGuess()
+    {
+        //guess = (max + min) / 2;
+        guess = Random.Range(min, max + 1);
+        //Converts guess string to text
+        guessText.text = guess.ToString();
+        maxNumberOfGuesses--;
+
+        if(maxNumberOfGuesses == 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+    void StartGame()
+    {
+       
+        NextGuess();
+       
+    }
+
+    public void GuessHigher() {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower() {
+        max = guess;
+        NextGuess();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        NextGuess();
+    }
+
+	
+}
